d05f8e969e99aec0f0e6c060732df61924fd7fa2
Karlo Kolak <97731344+karilca@users.noreply.github.com>
2026-02-22 16:38:49 +0100

    Dodavanje PWA-a!

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { registerSW } from 'virtual:pwa-register'
 
 import { queryClient } from './config/queryClient'
 import './index.css'
 import App from './App.tsx'
+
+// Register service worker for PWA
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
